cdbad3bf843fd9d4fae34e6ea360da406b06930c
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2023-04-21 21:36:01 -0300

    projeto inicial

---
+++ src/main/kotlin/br/com/market/adminatrator/AdministratorApplication.kt
@@ -0,0 +1,11 @@
+package br.com.market.adminatrator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdministratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdministratorApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/market/adminatrator/AdministratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.market.adminatrator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdministratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
